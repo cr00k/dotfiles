96ceaaf3b3aa1732853f5fea2731728f8e3a8d8e
cr00k <59626751+cr00k@users.noreply.github.com>
2023-10-30 20:42:30 +0100

    Update 2-install-hyprland.sh

--- 2-install-hyprland.sh
+++ 2-install-hyprland.sh
@@ -72,9 +72,9 @@ packagesYay=(
 _installPackagesPacman "${packagesPacman[@]}";
 _installPackagesYay "${packagesYay[@]}";
 
-#sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
-#sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
-#echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf &>> $INSTLOG
+sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
+echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf &>> $INSTLOG
 
 echo ""
 echo "DONE!"
