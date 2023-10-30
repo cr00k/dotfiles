5dbf9ab03ff3948c5d43c28777f2818ffefc82be
cr00k <59626751+cr00k@users.noreply.github.com>
2023-10-30 19:58:13 +0100

    Add files via upload

---
+++ 1-install.sh
@@ -0,0 +1,222 @@
+#bin/bash
+#  ___           _        _ _  
+# |_ _|_ __  ___| |_ __ _| | | 
+#  | || '_ \/ __| __/ _` | | | 
+#  | || | | \__ \ || (_| | | | 
+# |___|_| |_|___/\__\__,_|_|_| 
+#                              
+# ----------------------------------------------------- 
+# Install Script for required packages
+# ------------------------------------------------------
+
+# ------------------------------------------------------
+# Load Library
+# ------------------------------------------------------
+source $(dirname "$0")/scripts/library.sh
+clear
+echo "  ___           _        _ _  "
+echo " |_ _|_ __  ___| |_ __ _| | | "
+echo "  | ||  _ \/ __| __/ _  | | | "
+echo "  | || | | \__ \ || (_| | | | "
+echo " |___|_| |_|___/\__\__,_|_|_| "
+echo "                              "
+echo "-------------------------------------"
+echo ""
+
+# ------------------------------------------------------
+# Check if yay is installed
+# ------------------------------------------------------
+if sudo pacman -Qs yay > /dev/null ; then
+    echo "yay is installed. You can proceed with the installation"
+else
+    echo "yay is not installed. Will be installed now!"
+    _installPackagesPacman "base-devel"
+    git clone https://aur.archlinux.org/yay-git.git ~/yay-git
+    cd ~/yay-git
+    makepkg -si
+    cd ~/dotfiles/
+    clear
+    echo "yay has been installed successfully."
+    echo ""
+    echo "  ___           _        _ _  "
+    echo " |_ _|_ __  ___| |_ __ _| | | "
+    echo "  | ||  _ \/ __| __/ _  | | | "
+    echo "  | || | | \__ \ || (_| | | | "
+    echo " |___|_| |_|___/\__\__,_|_|_| "
+    echo "                              "
+    echo "-------------------------------------"
+    echo ""
+fi
+
+# ------------------------------------------------------
+# Confirm Start
+# ------------------------------------------------------
+
+while true; do
+    read -p "DO YOU WANT TO START THE INSTALLATION NOW? (Yy/Nn): " yn
+    case $yn in
+        [Yy]* )
+            echo "Installation started."
+        break;;
+        [Nn]* ) 
+            exit;
+        break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# ------------------------------------------------------
+# Install required packages
+# ------------------------------------------------------
+echo ""
+echo "-> Install main packages"
+
+packagesPacman=(
+    "pacman-contrib"
+    "alacritty"
+    "rofi" 
+    "firefox"
+    "chromium"
+    "nitrogen" 
+    "dunst" 
+    "starship"
+    "neovim"
+    "polkit-kde-agent"
+    "mpv" 
+    "freerdp" 
+    "xfce4-power-manager" 
+    "dolphin"
+    "dolphin-plugins"
+    "mousepad" 
+    "ttf-font-awesome" 
+    "ttf-fira-sans" 
+    "ttf-fira-code" 
+    "ttf-firacode-nerd" 
+    "figlet" 
+    "vlc" 
+    "exa" 
+    "python-pip" 
+    "python-psutil" 
+    "python-rich" 
+    "python-click" 
+    "xdg-desktop-portal-gtk"
+    "pavucontrol" 
+    "tumbler" 
+    "xautolock" 
+    "blueman"
+    "sddm"
+    "papirus-icon-theme"
+    "qt5-wayland"
+    "qt5ct"
+    "qt6-wayland"
+    "qt6ct"
+);
+
+packagesYay=(
+    "pfetch" 
+    "bibata-cursor-theme" 
+    "trizen"
+    "sddm-sugar-dark"
+#    "linux-headers"
+#    "nvidia-dkms"
+#    "nvidia-settings"
+#    "libva"
+#    "libva-nvidia-driver-git"
+);
+
+
+# ------------------------------------------------------
+# Install required packages
+# ------------------------------------------------------
+_installPackagesPacman "${packagesPacman[@]}";
+_installPackagesYay "${packagesYay[@]}";
+
+# ------------------------------------------------------
+# Install pywal
+# ------------------------------------------------------
+if [ -f /usr/bin/wal ]; then
+    echo "pywal already installed."
+else
+    yay --noconfirm -S pywal
+fi
+
+clear
+
+# ------------------------------------------------------
+# Install .bashrc
+# ------------------------------------------------------
+echo ""
+echo "-> Install .bashrc"
+
+_installSymLink .bashrc ~/.bashrc ~/dotfiles/.bashrc ~/.bashrc
+
+# ------------------------------------------------------
+# Install sddm display manager
+# ------------------------------------------------------
+echo ""
+echo "-> Install sddm display manager"
+while true; do
+    read -p "Do you want to install the custom login promt? (Yy/Nn): " yn
+    case $yn in
+        [Yy]* )
+            sudo systemctl enable sddm.service
+        break;;
+        [Nn]* ) 
+            echo "sddm installation skipped."
+        break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# ------------------------------------------------------
+# Install wallpapers
+# ------------------------------------------------------
+echo ""
+echo "-> Install wallapers"
+while true; do
+    read -p "Do you want to clone the wallpapers? If not, the script will install 3 default wallpapers to ~/wallpaper/ (Yy/Nn): " yn
+    case $yn in
+        [Yy]* )
+            if [ -d ~/wallpaper/ ]; then
+                echo "wallpaper folder already exists."
+            else
+                git clone https://github.com/cr00k/wallpapers.git ~/wallpaper
+                echo "wallpaper installed."
+            fi
+            echo "Wallpaper installed."
+        break;;
+        [Nn]* ) 
+            if [ -d ~/wallpaper/ ]; then
+                echo "wallpaper folder already exists."
+            else
+                mkdir ~/wallpaper
+            fi
+            cp ~/dotfiles/wallpapers/* ~/wallpaper
+            echo "Default wallpapers installed."
+        break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# ------------------------------------------------------
+# Init pywal
+# ------------------------------------------------------
+echo ""
+echo "-> Init pywal"
+wal -i ~/dotfiles/wallpapers/default.jpg
+echo "pywal initiated."
+
+# ------------------------------------------------------
+# Copy default wallpaper to .cache
+# ------------------------------------------------------
+echo ""
+echo "-> Copy default wallpaper to .cache"
+cp ~/dotfiles/wallpapers/default.jpg ~/.cache/current_wallpaper.jpg
+echo "default wallpaper copied."
+
+# ------------------------------------------------------
+# DONE
+# ------------------------------------------------------
+clear
+echo "DONE!" 
+echo "NEXT: Please continue with 2-install-hyprland.sh and/or 2-install-qtile.sh"
---
+++ 2-install-hyprland.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#  _   _                  _                 _  
+# | | | |_   _ _ __  _ __| | __ _ _ __   __| | 
+# | |_| | | | | '_ \| '__| |/ _` | '_ \ / _` | 
+# |  _  | |_| | |_) | |  | | (_| | | | | (_| | 
+# |_| |_|\__, | .__/|_|  |_|\__,_|_| |_|\__,_| 
+#        |___/|_|                              
+#  
+# ----------------------------------------------------- 
+# Install Script for Hyprland
+# ------------------------------------------------------
+
+# ------------------------------------------------------
+# Confirm Start
+# ------------------------------------------------------
+source $(dirname "$0")/scripts/library.sh
+clear
+echo "  _   _                  _                 _  "
+echo " | | | |_   _ _ __  _ __| | __ _ _ __   __| | "
+echo " | |_| | | | | ,_ \| ,__| |/ _\ | ,_ \ / _, | "
+echo " |  _  | |_| | |_) | |  | | (_| | | | | (_| | "
+echo " |_| |_|\__, | .__/|_|  |_|\__,_|_| |_|\__,_| "
+echo "        |___/|_|                              "
+echo "                             " 
+echo "------------------------------------------------------"
+echo ""
+
+while true; do
+    read -p "DO YOU WANT TO START THE INSTALLATION NOW? (Yy/Nn): " yn
+    case $yn in
+        [Yy]* )
+            echo "Installation started."
+        break;;
+        [Nn]* ) 
+            exit;
+        break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+echo ""
+
+# ------------------------------------------------------
+# Install required packages
+# ------------------------------------------------------
+echo ""
+echo "-> Install main packages"
+packagesPacman=(
+#    "hyprland-nvidia"
+    "hyprland"
+    "xdg-desktop-portal-wlr" 
+    "waybar" 
+    "grim" 
+    "slurp"
+    "swayidle"
+    "swappy"
+    "cliphist"
+);
+
+packagesYay=(
+    "swww" 
+    "swaylock-effects" 
+    "wlogout"
+);
+
+
+
+# ------------------------------------------------------
+# Install required packages
+# ------------------------------------------------------
+
+
+_installPackagesPacman "${packagesPacman[@]}";
+_installPackagesYay "${packagesYay[@]}";
+
+#sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+#sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
+#echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf &>> $INSTLOG
+
+echo ""
+echo "DONE!"
+echo "NEXT: Update the keyboard layout and screen resolution in ~/dotfiles/hypr/hyprland.conf"
+echo "Then proceed with with 3-dotfiles.sh"
+
